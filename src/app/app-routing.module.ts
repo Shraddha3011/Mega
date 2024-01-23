fe2747d57e94ebd1830b357475620ec8e255489c
Shraddha Jadhav <jadhavshraddha3011@gmail.com>
2024-01-23 17:10:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { QuestionComponent } from './components/question/question.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Q2Component } from './components/q2/q2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HeaderComponent},
+  {path:'signin',component:SignInComponent},
+  {path:'que',component:QuestionComponent},
+  {path:'q2',component:Q2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { QuestionComponent } from './components/question/question.component';
+import { Q2Component } from './components/q2/q2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SignInComponent,
+    QuestionComponent,
+    Q2Component
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,FormsModule,
     AppRoutingModule
   ],
   providers: [],
