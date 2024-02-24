8f74fc4729d91d4e0c6f8cabe7dfe423f069ff3d
Piyusha Patil <piyushapatil9585@gmail.com>
2024-02-24 15:47:18 +0530

    Change font, color and create new header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { QuestionComponent } from './components/question/question.component';
-import { HeaderComponent } from './components/header/header.component';
 import { Q2Component } from './components/q2/q2.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path:'',component:HeaderComponent},
+  {path:'',component:HomeComponent},
   {path:'signin',component:SignInComponent},
-  {path:'que',component:QuestionComponent},
+  {path:'question',component:QuestionComponent},
   {path:'q2',component:Q2Component},
   {path:'login',component:LoginComponent}
 ];
