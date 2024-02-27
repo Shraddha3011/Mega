5920aff9a66161e8355c01b54052561eb14abdd6
Aditi <aditikawade09@gmail.com>
2024-02-27 14:39:15 +0530

    Home_page1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+// home.component.ts
+
+import { AfterViewInit, Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.playAnimation();
+  }
+
+  private playAnimation() {
+    const elements = this.el.nativeElement.querySelectorAll('.welcome-text h1 .insura, .welcome-text h1 .match');
+
+    elements.forEach((element: HTMLElement) => {
+      this.renderer.removeClass(element, 'animated');
+      void element.offsetWidth; // Trigger a reflow to restart the animation
+      this.renderer.addClass(element, 'animated');
+    });
+  }
 }
